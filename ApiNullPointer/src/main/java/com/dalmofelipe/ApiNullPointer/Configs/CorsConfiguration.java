2f3ed33546da71fc6517eca16e0eeeaebbe9e7d1
Dalmo Felipe <dalmo.felipe@gmail.com>
2024-06-19 12:19:42 -0300

    api: habilita cors para consumo do webapp

---
+++ ApiNullPointer/src/main/java/com/dalmofelipe/ApiNullPointer/Configs/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.dalmofelipe.ApiNullPointer.Configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${environment.urls.webapp}")
+    private String webappBaseURL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(this.webappBaseURL)
+            .allowedMethods("GET", "POST");
+    }
+}
--- ApiNullPointer/src/main/java/com/dalmofelipe/ApiNullPointer/Entities/Pergunta.java
+++ ApiNullPointer/src/main/java/com/dalmofelipe/ApiNullPointer/Entities/Pergunta.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -22,7 +24,11 @@ public class Pergunta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
+
+    @Lob
+    @Column(columnDefinition = "TEXT", length = 1000)
     private String mensagem;
+
     private Integer visualizacoes;
     private Integer votos;
     private LocalDateTime data;
